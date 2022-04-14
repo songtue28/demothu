b89437336c9c606cb1fbbf35afe9b5877ff5b8a5
ThanhNga29 <lethithanhnga291202@gmail.com>
2022-04-14 12:20:38 +0700

    change tue

---
+++ CODELEARN.cpp
@@ -0,0 +1,33 @@
+//cout<<thanhnga; 
+#include<iostream>
+using namespace std;
+class ArrayCalculator
+{
+	public:
+    static int sumOfArray(int arr[], int n)
+    {
+        int count = 0;
+        for(int i = 0; i<n; i++)
+            {
+                count = count + arr[i];
+            }
+            return count;
+    }
+    static double sumOfArray(double arr[], int n)
+    {
+        double sum = 0;
+        for(int i=0; i<n; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+    }
+};
+int main()
+{
+	int arr1[]= {3, 4, 2};
+	double arr2[]= {1.3, 4.2, 6.7};
+	cout<<ArrayCalculator::sumOfArray(arr1, sizeof(arr1))/sizeof(int)<<endl;
+	cout<<ArrayCalculator::sumOfArray(arr2, sizeof(arr2)/sizeof(double))<<endl;
+	return 0;
+}
